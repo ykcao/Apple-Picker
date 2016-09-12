05dc69a8ef6e467356dc1f87cc83e85479982fb7
Yunke Cao <ykcao@umich.edu>
2016-09-12 08:16:29 -0400

    first commit

---
+++ Assets/Apple.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Apple : MonoBehaviour {
+	public static float bottomY = -20f;
+
+	void Update () {
+		if (transform.position.y < bottomY) {
+			Destroy (this.gameObject);
+			ApplePicker apScript = Camera.main.GetComponent<ApplePicker> ();
+			apScript.AppleDestroyed ();
+		}
+	}
+}
---
+++ Assets/HighScore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScore : MonoBehaviour {
+	static public int score = 1000;
+
+	void Awake(){
+		if (PlayerPrefs.HasKey ("ApplePickerHighScore")) {
+			score = PlayerPrefs.GetInt("ApplePickerHighScore");
+		}
+		PlayerPrefs.SetInt ("ApplePickerHighScore", score);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GUIText gt = this.GetComponent<GUIText> ();
+		gt.text = "High Score: " + score;
+		if (score > PlayerPrefs.GetInt("ApplePickerHighScore")){
+			PlayerPrefs.SetInt ("ApplePickerHighScore", score);
+		}
+	}
+}
